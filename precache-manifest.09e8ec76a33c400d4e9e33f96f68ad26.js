ad29a712f4d4aed45ba31b347d21b64e469f8e0b
Miloš Pavlović <miloskince@gmail.com>
2019-12-22 22:26:52 +0100

    Initial Commit

---
+++ precache-manifest.09e8ec76a33c400d4e9e33f96f68ad26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505c561a4cca42bc7751b6dca8b52518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3db11344ee3e7632f02b",
+    "url": "/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "4ee4671c3a39513893d8",
+    "url": "/static/css/main.e40216b1.chunk.css"
+  },
+  {
+    "revision": "3db11344ee3e7632f02b",
+    "url": "/static/js/2.97e3afc0.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.97e3afc0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ee4671c3a39513893d8",
+    "url": "/static/js/main.757547de.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  }
+]);
